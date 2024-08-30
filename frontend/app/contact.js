535e57e1e14ca4e5d162fc5cf8e3e8e9ac1cc2f9
Ammarkhan96 <ammarkhaan96@gmail.com>
2024-08-30 16:20:57 +0500

    first commit

---
+++ frontend/app/contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className="container mx-auto px-4">Contact Us</div>
+  )
+}
+
+export default Contact
---
+++ frontend/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="container mx-auto px-4">
+       This is a Home-Page
+      </div>
+  );
+}
